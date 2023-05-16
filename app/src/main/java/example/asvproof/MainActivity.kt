79972d96357dd22a784a5cbffd75fff729dc4d33
wangjian <2307742375@qq.com>
2023-05-16 12:01:37 +0800

    1

--- app/src/main/java/example/asvproof/MainActivity.kt
+++ app/src/main/java/example/asvproof/MainActivity.kt
@@ -1,46 +1,64 @@
 package example.asvproof
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import example.asvproof.ui.theme.AsvproofTheme
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import example.asvproof.R
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private val client = OkHttpClient()
+
+    // 创建一个用于更新UI的Handler
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AsvproofTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        val textView: TextView = findViewById(R.id.textView)
+        button.setOnClickListener {
+            sendRequest(textView)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun sendRequest(textView: TextView) {
+        val request = Request.Builder()
+            .url("https://www.baidu.com")
+            .build()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AsvproofTheme {
-        Greeting("Android")
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // 将错误信息显示在TextView上
+                handler.post {
+                    textView.text = "请求失败: ${e.message}"
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val bodyString = response.body?.string()
+                    // 将响应的主体显示在TextView上
+                    handler.post {
+                        textView.text = "请求成功: $bodyString"
+                    }
+                } else {
+                    // 将错误信息显示在TextView上
+                    handler.post {
+                        textView.text = "服务器错误: ${response.code}"
+                    }
+                }
+            }
+        })
     }
 }
+
